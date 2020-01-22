dccec9b14910c05eefdf0f7b2bb328f19da6c4ff
Nacho Verdón <nverblaz@gmail.com>
2020-01-22 15:05:39 +0100

    - Add WkHtmlToPdf documentation

--- src/main/java/com/nachoverdon/mongolia/pdf/WkHtmlToPdf.kt
+++ src/main/java/com/nachoverdon/mongolia/pdf/WkHtmlToPdf.kt
@@ -12,6 +12,28 @@ class WkHtmlToPdf {
     /**
      * You can find documentation on wkhtmltopdf on https://wkhtmltopdf.org/docs.html
      *
+     * Images:
+     * Images cannot be directly linked, they need to be encoded to Base64 and added inline. To accomplish this, you can
+     * use resourcesImageToBase64() and damImageToBase64() from MongoTemplatingFunctions.
+     *
+     * CSS:
+     * Similarly, CSS must also be embedded using <style> stags instead of linked.
+     * wkhtmltopdf uses Qt 4.8.* (Qt WebKit) rendering engine internally to render the page. Some CSS3 features, like
+     * flexbox, are NOT supported by default. You might need to use older syntax. More info:
+     *      https://github.com/wkhtmltopdf/wkhtmltopdf/issues/1522#issuecomment-159767618
+     *      https://developer.mozilla.org/en-US/docs/Web/CSS/box-align
+     *
+     * Headers/Footers:
+     * These should be .html files and must be accessible from the webapp module, so put them on your resources folder.
+     * Ex: /yourmodule/src/main/resources/mgnl-resources/yourmodule/header.thml
+     *
+     * Fonts:
+     * Fonts should be installed on the machine, or they wont load properly. Remember to install them on your production
+     * environment as well.
+     *
+     * Note:
+     * If you have the time, checkout Puppeteer (https://pptr.dev), which might be a better alternative to wkhtmltopdf
+     *
      * @param html The HTML you want to convert to PDF
      * @param parameters A list of wkhtmltopdf parameters. ex: Arrays.asList("--header-html", "my/path/header.html");
      * @return
